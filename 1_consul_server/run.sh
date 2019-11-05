8a5d1342fd8a8fcd86b25382e2fe0d35c87ae103
Ubuntu <hyeongchae@gmail.com>
2019-11-05 08:10:53 +0000

    modify hostname

--- 1_consul_server/run.sh
+++ 1_consul_server/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export BIND="0.0.0.0"
 export CONTAINER="consul-01"
-export HOSTNAME="$CONTAINER.example.com"
+export HOSTNAME="$CONTAINER.example.test"
 
 docker stop $CONTAINER 2>&1
 docker rm   $CONTAINER 2>&1
@@ -11,13 +12,13 @@ docker run --name  $CONTAINER \
 	--net marvel_dc \
 	--dns 127.0.0.1 \
 	--ip 172.16.10.1 \
-	--publish 8300:8300 \
-	--publish 8301:8301 \
-	--publish 8301:8301/udp \
-	--publish 8302:8302 \
-	--publish 8302:8302/udp \
-	--publish 8500:8500 \
-	--publish 8600:8600 \
-	--publish 8600:8600/udp \
+	--publish ${BIND}:8300:8300 \
+	--publish ${BIND}:8301:8301 \
+	--publish ${BIND}:8301:8301/udp \
+	--publish ${BIND}:8302:8302 \
+	--publish ${BIND}:8302:8302/udp \
+	--publish ${BIND}:8500:8500 \
+	--publish ${BIND}:8600:8600 \
+	--publish ${BIND}:8600:8600/udp \
 	--detach \
 	consul-server
