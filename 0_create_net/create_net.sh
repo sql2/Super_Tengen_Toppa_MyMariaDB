1fa832c0593454f6122f47771996a877acc50298
Ubuntu <hyeongchae@gmail.com>
2019-11-11 08:52:09 +0000

    modfiy

--- 0_create_net/create_net.sh
+++ 0_create_net/create_net.sh
@@ -5,7 +5,7 @@ docker network rm marvel_dc
 docker network create \
                 --opt "com.docker.network.bridge.name"="marvel_dc" \
                 --driver=bridge \
-                --subnet=172.16.0.0/16 \
                 --ip-range=172.16.1.1/24 \
+                --subnet=172.16.0.0/16 \
                 --gateway=172.16.1.254 \
                 marvel_dc
