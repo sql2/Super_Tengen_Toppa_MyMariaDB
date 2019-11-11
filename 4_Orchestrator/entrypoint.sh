c5ed8babab2eed6b1178ba5a44ebee4b9160010d
Ubuntu <hyeongchae@gmail.com>
2019-11-11 10:54:34 +0000

    modify jq

--- 4_Orchestrator/entrypoint.sh
+++ 4_Orchestrator/entrypoint.sh
@@ -12,7 +12,7 @@ echo '[Entrypoint] Validated consul.'
 consul validate -quiet /etc/consul.d
 
 echo '[Entrypoint] Start consul agent.'
-consul agent -config-file=/etc/consul.d/config.json &
+consul agent -config-file=/etc/consul.d/consul_client.json &
 
 echo '[Entrypoint] Start Orchestrator.'
 cd /usr/local/orchestrator && ./orchestrator --config=/etc/orchestrator.conf.json http
--- 4_Orchestrator/run.sh
+++ 4_Orchestrator/run.sh
@@ -10,7 +10,7 @@ docker rm   $CONTAINER 2>&1
 docker run --name $CONTAINER \
 		--hostname $HOSTNAME \
 		--net marvel_dc \
-		--dns 127.0.0.1
+		--dns 127.0.0.1 \
 		--ip 172.16.40.1 \
 		--publish ${BIND}:3000:3000 \
 		--detach \
