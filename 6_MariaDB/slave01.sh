8459034c5b627149c206a7f89157fb2a1a28c4a7
Ubuntu <hyeongchae@gmail.com>
2019-11-12 08:50:18 +0000

    add 5,6,7

---
+++ 6_MariaDB/slave01.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export BIND="0.0.0.0"
+export CONTAINER="slave01"
+export HOSTNAME="$CONTAINER.example.test"
+
+docker stop $CONTAINER 2>&1
+docker rm   $CONTAINER 2>&1
+
+docker run --name  $CONTAINER \
+	--hostname $HOSTNAME \
+	--net marvel_dc \
+	--dns 127.0.0.1 \
+	--ip 172.16.60.2 \
+	--publish 3306:3306 \
+	--detach \
+	mariadb-server
+
---
+++ 6_MariaDB/slave02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export BIND="0.0.0.0"
+export CONTAINER="slave02"
+export HOSTNAME="$CONTAINER.example.test"
+
+docker stop $CONTAINER 2>&1
+docker rm   $CONTAINER 2>&1
+
+docker run --name  $CONTAINER \
+	--hostname $HOSTNAME \
+	--net marvel_dc \
+	--dns 127.0.0.1 \
+	--ip 172.16.60.3 \
+	--publish 3306:3306 \
+	--detach \
+	mariadb-server
+
