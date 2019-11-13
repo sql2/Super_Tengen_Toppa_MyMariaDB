8d7ade748e74e8bfbeef34131cadbe4416a42109
Ubuntu <hyeongchae@gmail.com>
2019-11-13 00:54:38 +0000

    modify

--- 7_ProxySQL/run.sh
+++ 7_ProxySQL/run.sh
@@ -6,7 +6,8 @@ docker rm   proxysql 2>&1
 docker run --name proxysql \
 		--hostname proxysql \
 		--net marvel_dc \
-		--ip 172.16.50.1 \
+		--dns 127.0.0.1
+		--ip 172.16.70.1 \
 		--publish 3366:3306 \
 		--publish 6032:6032 \
 		--publish 6080:6080 \
