152c70c32185cd6afbbde6c23ff9d8d7c6676c26
sql2 <hyeongchae@gmail.com>
2019-11-28 11:10:14 +0000

    modify

--- 8_Bench/run.sh
+++ 8_Bench/run.sh
@@ -5,41 +5,48 @@ export MYSQL_PWD="sandbox"
 while true
 do
 	# proxysql - insert
+	echo -n "ProxySQL : "
 	mysql --silent --skip-column-names \
 		-h 127.0.0.1 \
 		-P 3366 \
 		-usandbox \
 		-e "INSERT INTO dummy.tbl VALUES ( 1 );"
 
-    # master direct
+	# master direct
+	echo -n "Master   : "
 	mysql --silent --skip-column-names \
 		-h 127.0.0.1 \
 		-P 3306 \
 		-usandbox \
-		-e "SELECT '  direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
+		-e "SELECT 'direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
 
 	# slave-1 direct
+	echo -n "Slave01  : "
 	mysql --silent --skip-column-names \
 		-h 127.0.0.1 \
 		-P 3307 \
 		-usandbox \
-		-e "SELECT '  direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
+		-e "SELECT 'direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
 	
 	# slave-2 direct
+	echo -n "Slave02  : "
 	mysql --silent --skip-column-names \
 		-h 127.0.0.1 \
 		-P 3308 \
 		-usandbox \
-		-e "SELECT '  direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
+		-e "SELECT 'direct ->', @@hostname, COUNT(*) FROM dummy.tbl;"
 	
 	# proxysql - select
+	echo -n "ProxySQL : "
 	mysql --silent --skip-column-names \
 		-h 127.0.0.1 \
 		-P 3366 \
 		-usandbox \
 		-e "SELECT 'proxysql ->', @@hostname, COUNT(*) FROM dummy.tbl;"
 
-	echo "----------------------------------------------"
+	echo ""
+	echo "--------------------------------------------------------------------------------"
+	echo ""
 
-	sleep 1
+	sleep 3 
 done
