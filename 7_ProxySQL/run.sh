29a74dafee665b9497e02cc576a1d0e66b731924
sql2 <hyeongchae@gmail.com>
2019-11-30 03:54:24 +0000

    modify

--- 7_ProxySQL/run.sh
+++ 7_ProxySQL/run.sh
@@ -10,7 +10,7 @@ docker rm   $CONTAINER 2>&1
 docker run --name $CONTAINER \
 	--hostname $HOSTNAME \
 	--net marvel_dc \
-	--dns 127.0.0.1 \
+	--dns-search "example.test" \
 	--ip 172.16.70.1 \
 	--publish ${BIND}:3366:3306 \
 	--publish ${BIND}:6032:6032 \
